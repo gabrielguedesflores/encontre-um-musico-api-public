63d820f51cd54cb6c05f5da16f5f1f7d11e35ad6
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2022-02-01 21:52:01 -0300

    add field user_active in endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,16 +3,16 @@ const db = require("../config/database");
 // ==> Método responsável por criar um novo 'User':
 
 exports.createUser = async (req, res) => {
-  const { user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram } = req.body;
+  const { user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram, user_active } = req.body;
   const { rows } = await db.query(
-    "INSERT INTO users (user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
-    [user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram]
+    "INSERT INTO users (user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram, user_active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+    [user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram, user_active]
   );
 
   res.status(201).send({
     message: "User added successfully!",
     body: {
-      user: { user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram }
+      user: { user_full_name, user_name, user_email, user_pass, user_bio, user_state, user_city, user_image, user_twitter, user_facebook, user_instagram, user_active }
     },
   });
 };
@@ -20,6 +20,13 @@ exports.createUser = async (req, res) => {
 // ==> Método responsável por listar todos os 'Users':
 exports.listAllUsers = async (req, res) => {
   const response = await db.query('SELECT * FROM users ORDER BY user_full_name ASC');
+  res.status(200).send(response.rows);
+};
+
+// ==> Método responsável por listar todos os 'Users' ativos:
+exports.listAllUsersActive = async (req, res) => {
+  const user_active = parseInt(req.params.user_active);
+  const response = await db.query('SELECT * FROM users WHERE user_active = $1 ORDER BY user_full_name ASC', [user_active]);
   res.status(200).send(response.rows);
 };
 
@@ -43,3 +50,9 @@ exports.loginToUser = async (req, res) => {
     },
   });
 };
+
+exports.alterActiveUser = async (req, res) => {
+  const user_id = parseInt(req.params.user_id);
+  const response = await db.query('SELECT * FROM users WHERE user_id = $1', [user_id]);
+  res.status(200).send(response.rows);
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,11 +9,16 @@ router.post('/user', userController.createUser);
 // ==> Rota responsável por listar todos os 'Users': (GET): localhost:3000/api/users
 router.get('/users', userController.listAllUsers);
 
+// ==> Rota responsável por listar todos os 'Users' ativos: (GET): localhost:3000/api/users
+router.get('/users/:user_active', userController.listAllUsersActive);
 
 // ==> Rota responsável por selecionar 'User' pelo 'user_id': (GET): localhost:3000/api/users/:user_id
 router.get('/users/:user_id', userController.findUserById);
 
 // ==> Rota responsável por logar via API: (POST): localhost:3000/api/users/login
 router.post('/users/login', userController.loginToUser);
+
+// ==> Rota responsável por inativar um usuário via API: (PUT): localhost:3000/api/user
+router.put('/user/delete/:user:id', userController.alterActiveUser);
 
 module.exports = router;
